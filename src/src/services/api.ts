ed530174aeced1b41c0eec49d7673661a08dc127
trulsasb <trulsasb@gmail.com>
2026-01-26 10:27:55 +0100

    Delete src/src/services directory
    Delete incorrectly nested

--- src/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-const BASE_URL = import.meta.env.VITE_API_URL;
-
-export async function fetchProducts() {
-  const res = await fetch(`${BASE_URL}/api/products`);
-  if (!res.ok) {
-    throw new Error("Kunne ikke hente produkter");
-  }
-  return res.json();
-}
