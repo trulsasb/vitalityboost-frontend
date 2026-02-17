c61401f58cd32a2168ca34008e328cb7ea487338
trulsasb <trulsasb@gmail.com>
2026-02-17 09:20:29 +0100

    Delete src/services/api.ts

--- src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_URL = "https://vitalityboost-backend-2.onrender.com"; // backend URL
-
-export async function fetchProducts() {
-  const response = await axios.get(`${API_URL}/products`);
-  return response.data;
-}
