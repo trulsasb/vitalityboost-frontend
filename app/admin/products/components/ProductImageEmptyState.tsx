525736cf3b6af11262135264912efacc0d8f73c7
trulsasb <trulsasb@gmail.com>
2026-05-26 20:27:07 +0200

    Delete app/admin/products/components/ProductImageEmptyState.tsx

--- app/admin/products/components/ProductImageEmptyState.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function ProductImageEmptyState() {
-  return (
-    <div className="border-2 border-dashed border-gray-300 rounded p-8 text-center text-gray-500">
-      Ingen bilder lastet opp ennå
-    </div>
-  );
-}
