8b05c240b297f4f6ad5eac11385cb38e41a2fc94
trulsasb <trulsasb@gmail.com>
2026-05-26 19:22:48 +0200

    Create ProductImageEmptyState.tsx

---
+++ app/admin/products/components/ProductImageEmptyState.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ProductImageEmptyState() {
+  return (
+    <div className="border-2 border-dashed border-gray-300 rounded p-8 text-center text-gray-500">
+      Ingen bilder lastet opp ennå
+    </div>
+  );
+}
