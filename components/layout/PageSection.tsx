a72f484be791608f159bda8c8c38de6d2982f62b
trulsasb <trulsasb@gmail.com>
2026-05-28 14:41:35 +0200

    Update PageSection.tsx

--- components/layout/PageSection.tsx
+++ components/layout/PageSection.tsx
@@ -1,9 +1,14 @@
+import * as React from "react";
+
 interface PageSectionProps {
   children: React.ReactNode;
   className?: string;
 }
 
-export function PageSection({ children, className = "" }: PageSectionProps) {
+export default function PageSection({
+  children,
+  className = "",
+}: PageSectionProps) {
   return (
     <section className={`w-full mb-12 ${className}`}>
       {children}
