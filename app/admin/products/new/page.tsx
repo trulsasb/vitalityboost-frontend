12f1cc2113eec14d5cb88caabd52d2069eb306c9
trulsasb <trulsasb@gmail.com>
2026-05-31 19:30:42 +0200

    Update page.tsx

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -136,5 +136,7 @@ export default function NewProductPage() {
             className="border border-gray-300 rounded-md px-3 py-2 w-full"
           />
         </div>
-
-        <div>
+      </form>
+    </div>
+  );
+}
