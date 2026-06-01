60d884480b5a9497e2c5d59eb8db234eb04175c4
trulsasb <trulsasb@gmail.com>
2026-06-01 10:32:16 +0200

    Add cn function to filter and join class names

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,2 +1,6 @@
-export * from "./utils/calc"
-export * from "./utils/format"
+export function cn(...classes: (string | false | null | undefined)[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export * from "./utils/calc";
+export * from "./utils/format";
