4c4a17f4f81c3e5289f87ea2d598d860c4fdfbf6
trulsasb <trulsasb@gmail.com>
2026-06-02 13:27:50 +0200

    Change CartSummary import to named import

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -2,7 +2,7 @@
 
 import { useCart } from "@/app/cart/CartProvider";
 import CartItem from "@/app/cart/Drawer/CartItem";
-import CartSummary from "@/app/cart/CartSummary";
+import { CartSummary } from "@/app/cart/CartSummary";
 import { useState } from "react";
 
 export default function CheckoutPage() {
