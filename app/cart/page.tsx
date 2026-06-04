7936f5282f579814f82987600773e6f0d21453b3
trulsasb <trulsasb@gmail.com>
2026-06-04 13:12:55 +0200

    Add revalidate setting to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 import { useCart } from "@/app/cart/CartProvider";
 import CartItem from "@/app/cart/Drawer/CartItem";
