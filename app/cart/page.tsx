d29690fefbfaf019feac462ca418a54a74afd20b
trulsasb <trulsasb@gmail.com>
2026-06-04 12:48:39 +0200

    Force dynamic rendering for cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useCart } from "@/app/cart/CartProvider";
 import CartItem from "@/app/cart/Drawer/CartItem";
 import { CartSummary } from "@/app/cart/CartSummary";
@@ -34,3 +36,4 @@ export default function CartPage() {
     </div>
   );
 }
+
